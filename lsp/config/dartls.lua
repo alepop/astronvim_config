423ce2a4e236c2f07e4cbcc1381a83b3a431ade7
Aleksey Popov <alepooop@gmail.com>
2023-05-09 10:18:46 +0300

    chore(dart): update dart server settings

--- lsp/config/dartls.lua
+++ lsp/config/dartls.lua
@@ -1,5 +1,11 @@
 return {
   settings = {
-    lineLength = 100
-  }
+    lineLength = 100,
+    renameFilesWithClasses = 'always',
+    analysisExcludedFolders = {
+      vim.fn.expand("$HOME/.pub-cache"),
+      vim.fn.expand("$HOME/fvm/"),
+      vim.fn.expand("/opt/homebrew/"),
+    },
+  },
 }
